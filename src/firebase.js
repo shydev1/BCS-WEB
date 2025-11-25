a718df86a3c5df873bfee88810ef6abab20e6fdc
shydev1 <okonenin@gmail.com>
2025-11-25 09:18:33 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,6 @@ import { getFirestore, doc, setDoc, getDoc, updateDoc } from 'firebase/firestore
 import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 
 const firebaseConfig = {
-  // apiKey: "AIzaSyBlbmEz0BX3vaBdkiUZ6p_rlPNHPWvsvzg",
-  // authDomain: "scooter-beccf.firebaseapp.com",
-  // projectId: "scooter-beccf",
-  // storageBucket: "scooter-beccf.appspot.com",
-  // messagingSenderId: "370422506482",
-  // appId: "1:370422506482:web:16590e7abafdeb6628416b",
-  // measurementId: "G-QB9PQ4ZMM2"
 };
 
 const app = initializeApp(firebaseConfig);
